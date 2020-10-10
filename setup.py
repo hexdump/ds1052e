634264a531dcc009f50f9af85a95d297cac32cae
hexdump <contact@hexdump.email>
2020-10-10 13:28:00 -0500

    reorganization, and clean data output (remove first samples and make time and data same size)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ds1052e',
-    version='1.0.0',
+    version='1.0.3',
     author='Leslie Schumm',
     author_email='contact@hexdump.email',
     packages=['ds1052e'],
